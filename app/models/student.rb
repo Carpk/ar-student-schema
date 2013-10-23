9dac22c1fbbe7f8ba4dfb04c624ab72d7f195784
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-23 18:14:47 -0500

    passing 7 tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,37 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+# raise "emailerror" unless
+  #validates :email if
+
+# @student.assign_attributes(:email => address)
+
+
+validates :email, :format => { :with => /.+@.+\.+.{2,}/,
+    :message => "Only letters allowed" }
+
+validates :email, :uniqueness => true
+
+validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+
+validates :phone, :format => { :with => /.?\d{3}.? \d{3}\D*\d{4}.?.*/,
+    :message => "Invalid Phone Number" }
+
+
+
+
+
+  # def email(address)
+  #   self.assign_attributes(:email => address) if address =~ /.+@.+\.+.{2,}/
+  # end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Date.today.year - birthday.year
+  end
+
 end
